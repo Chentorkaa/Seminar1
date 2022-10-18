4cf5e48bd4e9d513ab68868bbd86838f0baf984a
Chentorka <chentorka@gmail.com>
2022-10-18 15:05:33 +0300

    a*b

---
+++ Example1/Program.cs
@@ -0,0 +1,5 @@
+﻿// На входе число, на выходе число умноженное на само себя
+
+Console.WriteLine("Введите значение");
+int number = int.Parse(Console.ReadLine()!);
+Console.Write($"Квадрат числа = {number*number}");
