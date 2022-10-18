bcced2ec3f8f0b79f221af27ae287c4ecdace83d
Chentorka <chentorka@gmail.com>
2022-10-18 16:06:48 +0300

    square

---
+++ Example3/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите значение a");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение b");
+int b = int.Parse(Console.ReadLine());
+bool square = b == a*a;
+if (square);
+Console.WriteLine(square);
